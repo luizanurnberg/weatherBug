6fc9c6e2b4874f8b0fe841575bf22914c08363d2
Luíza <116217246+luizanurnberg@users.noreply.github.com>
2023-03-17 09:59:12 -0300

    Update Weather.js

--- model/Weather.js
+++ model/Weather.js
@@ -1,11 +1,13 @@
-//Defining that Weather has to receive 'data'
+/* - Defining that Weather has to receive 'data'
+   - An array of errors was used by the teacher as one of the multiple ways to aware the user that something went wrong
+*/
 const Weather = function (data) {
     this.data = data
     this.errors = []
 }
 
 /*If the property can't be found in the object itself, the prototype is searched for the property. 
- -If still not found, an error will be pushed to errors array and used in the controller's function 'exports.getWeather' 
+ - If still not found, an error will be pushed to errors array and used in the controller's function 'exports.getWeather' 
 */
 Weather.prototype.validateUserInput = function () {
     if (this.data == "") {
